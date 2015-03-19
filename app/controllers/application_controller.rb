12d85fa10e1b376bf28d70234b9f7255ab799ea6
rFang0414 <Riu.Fang@careerbuilder.com>
2015-03-19 23:19:24 +0800

    123

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,29 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+  require 'openssl'
+  require 'base64'
+  ALG = 'DES-EDE3-CBC'
+  KEY = 'uryeiyel'
+  DES_KEY = 'uygbfs3w'
+
+  def encode(str)
+    des = OpenSSL::Cipher::Cipher.new ALG
+    des.pkcs5_keyivgen KEY, DES_KEY
+    des.encrypt
+    cipher = des.update str
+    cipher << des.final
+    Base64.urlsafe_encode64 cipher
+  end
+
+  def decode(str)
+    str = Base64.urlsafe_decode64 str
+    des = OpenSSL::Cipher::Cipher.new ALG
+    des.pkcs5_keyivgen KEY, DES_KEY
+    des.decrypt
+    des.update(str) + des.final
+  end
+
 end
--- app/controllers/spread_controller.rb
+++ app/controllers/spread_controller.rb
@@ -1,8 +1,31 @@
 class SpreadController < ApplicationController
     def get_job
-        render :nothing => true
+      @job = Job.find_by(id: params[:id].to_i)
+      @node_id = params[:node_id].blank? ? encode("0") : params[:node_id]
     end
+
     def my_resume
-        render :nothing => true
+
     end
+
+    def share
+      user_phone = params[:telephone]
+      user_name = params[:name]
+      node_id = params[:node_id]
+      job_id = params[:job_id]
+      previous_id = decode(node_id)
+
+      node = Node.create(previous_id:previous_id,user_phone:user_phone,job_id:job_id)
+      new_node_id = encode(node.id.to_s)
+
+      Player.create(name:user_phone,phone:user_phone)
+
+      redirect_to '/spread/job/' + job_id + "?node_id=" + new_node_id
+    end
+
+
+    def generate_list_node
+
+    end
+
 end
